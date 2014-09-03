e37e8fe251874b11180c06fe901a96e444e4aefd
BobCochran <r2cochran2@gmail.com>
2014-09-02 21:14:51 -0400

    Use the script to test resizing images that are contained in buffers, and writing them out to the file system.

--- resize_with_gm.js
+++ resize_with_gm.js
@@ -8,11 +8,6 @@
  * { [Error: spawn ENOENT] code: 'ENOENT', errno: 'ENOENT', syscall: 'spawn' }
  *
  */
-var MongoClient = require('mongodb').MongoClient  // Driver for connecting to MongoDB
-    , format = require('util').format;
-
-var host = process.env['MONGO_NODE_DRIVER_HOST'] != null ? process.env['MONGO_NODE_DRIVER_HOST'] : 'localhost';
-var mongoport = process.env['MONGO_NODE_DRIVER_PORT'] != null ? process.env['MONGO_NODE_DRIVER_PORT'] : 27017;
 
 var fs = require('fs')
 var gm = require('gm')
@@ -20,14 +15,50 @@ var my_user = process.env['HOME']  //Mac OS X: the user's Home directory
 
 gm(my_user + '/Downloads/mongoimg/mongodb_presentation_images/IMG_1785.JPG')
 .options({imageMagick: true})
-.resize(850)
-.write(my_user + '/Downloads/mongoimg/mongodb_presentation_images/IMG_1785_resized_850.JPG', function (err) {
+.resize(650)
+.write(my_user + '/Downloads/mongoimg/mongodb_presentation_images/IMG_1785_resized_650.JPG', function (err) {
         if (err) {
 
             console.log(err)
         }
         if (!err) {
-            console.log("The image was resized.")
+            console.log("The image was resized to 650px successfully.")
         }
 
     })
+/* See if we can use readsFileSync, which reads the image into a Buffer, and then
+ * resize the image contained in the buffer first to 750px, and then to 650px, in separate
+ * processing with the gm utility. It seems best to wait 2 seconds for the above resize to process
+ *
+ */
+
+setTimeout(function() {
+    console.log("\nNow attempting to resize an image contained in a buffer which is then passed to gm")
+    var buf = fs.readFileSync(my_user + '/Downloads/mongoimg/mongodb_presentation_images/IMG_1786.JPG')
+    gm(buf, my_user + '/Downloads/mongoimg/mongodb_presentation_images/IMG_1786.JPG')
+     .options({imageMagick: true})
+     .resize(750)
+     .write(my_user + '/Downloads/mongoimg/mongodb_presentation_images/IMG_1786_resized_750.JPG', function (err) {
+            if (err) {
+                console.log(err)
+            }
+            if (!err) {
+                console.log('\nThe image was resized to 750px successfully.')
+            }
+        })
+
+    /* Now try a second resize action on the same buffer, but resize to 650px */
+
+    gm(buf, my_user + '/Downloads/mongoimg/mongodb_presentation_images/IMG_1786.JPG')
+        .options({imageMagick: true})
+        .resize(650)
+        .write(my_user + '/Downloads/mongoimg/mongodb_presentation_images/IMG_1786_resized_650.JPG', function (err) {
+            if (err) {
+                console.log(err)
+            }
+            if (!err) {
+                console.log('\nThe image was resized to 650px successfully.')
+            }
+        })
+
+}, 2000)
